e929ef9132a10738771ba49c927c9e2777771b76
tu <nguyenmanhtu09042000@gmail.com>
2020-05-30 01:27:15 +0700

    update-post

--- db/migrate/20200529091344_add_fields_to_u_sers.rb
+++
@@ -1,4 +0,0 @@
-class AddFieldsToUSers < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
---
+++ db/migrate/20200529174125_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.string :image_url
+
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
