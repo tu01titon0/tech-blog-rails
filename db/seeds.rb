1b369be08af9b12f76fc2baf5c71ef7fefae7911
tu <nguyenmanhtu09042000@gmail.com>
2020-05-30 12:04:37 +0700

    update-fix-issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create(
+  [
+    {email: "sample1@sample.com", password: "password", password_confirmation: "password"},
+    {email: "sample2@sample.com", password: "password", password_confirmation: "password"},
+    {email: "sample3@sample.com", password: "password", password_confirmation: "password"},
+  ]
+)
+
+users.each do |user|
+  10.times do |i|
+    Post.create(title: "Title - #{i}", content:"abc", image_url: "https://www.brainscape.com/blog/wp-content/uploads/2015/01/hires.jpg", user: user)
+  end
+end
